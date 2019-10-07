2a2782339baf6293ca14e23f87d372276c922252
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-07 14:04:29 +0200

    Add first step of feed cmd

--- pkg/commands/constants.go
+++ pkg/commands/constants.go
@@ -1,5 +1,17 @@
 package commands
 
+// colors
+const pendingColor = "#f9a41b"
+
 // cancel button
 const cancelText = "Cancel"
 const CancelCallback = "cancel-request"
+
+// feed command
+const feedError = "Can not start feeding people"
+const feedCmd = "feed"
+const feedDesc = "Pair group of people together for company paid lunch"
+const selectChannelQuestion = "📰 Which channel do you wanna link bot to?"
+const selectChannelCallbackId = "select-food-channel"
+const selectChannelPlaceholder = "pick a channel"
+const foodChannelKey = "food-channel"
---
+++ pkg/commands/feed.go
@@ -0,0 +1,55 @@
+package commands
+
+import (
+	"github.com/nlopes/slack"
+	"github.com/omaressameldin/lunch-roulette/internal/utils"
+	"github.com/shomali11/slacker"
+)
+
+func AddFeedCmd(bot *slacker.Slacker) {
+	bot.Command(feedCmd, feed())
+}
+
+func feed() *slacker.CommandDefinition {
+	return &slacker.CommandDefinition{
+		Description: feedDesc,
+		Handler: func(request slacker.Request, response slacker.ResponseWriter) {
+			channel := request.Event().Channel
+			rtm := response.RTM()
+
+			utils.ReplyWithError(selectFoodChannel(rtm, channel), feedError, response)
+		},
+	}
+}
+
+func selectFoodChannel(rtm *slack.RTM, channel string) error {
+	channels, err := rtm.GetChannels(true)
+	if err != nil {
+		return err
+	}
+
+	options := make([]slack.AttachmentActionOption, len(channels))
+	for _, channel := range channels {
+		options = append(options, slack.AttachmentActionOption{
+			Text:  channel.Name,
+			Value: channel.ID,
+		})
+	}
+
+	attachment := slack.Attachment{
+		Text:       selectChannelQuestion,
+		Color:      pendingColor,
+		CallbackID: selectChannelCallbackId,
+		Actions: []slack.AttachmentAction{
+			Select(
+				selectChannelPlaceholder,
+				foodChannelKey,
+				options,
+			),
+			CancelButton(),
+		},
+	}
+
+	rtm.PostMessage(channel, slack.MsgOptionAttachments(attachment))
+	return nil
+}
