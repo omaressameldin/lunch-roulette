84e8af509f3b64fdc5574c05d88c6d3b71d209f0
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-07 12:40:44 +0200

    Add main pkg to connect ot db and slack

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/omaressameldin/lunch-roulette/internal/env"
+import (
+	"log"
+
+	"github.com/omaressameldin/lunch-roulette/internal/db"
+	"github.com/omaressameldin/lunch-roulette/internal/env"
+	"github.com/omaressameldin/lunch-roulette/pkg/bot"
+)
 
 func main() {
 	env.ValidateEnvKeys()
+
+	d := db.OpenDB()
+	defer d.CloseDB()
+
+	b, err := bot.CreateBot(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.StartListening()
 }
