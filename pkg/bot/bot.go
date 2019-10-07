e1fcfa83583a21275151559d1565369e8affd48a
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-07 14:19:17 +0200

    Add bot pkg

---
+++ pkg/bot/bot.go
@@ -0,0 +1,41 @@
+package bot
+
+import (
+	"context"
+	"log"
+
+	"github.com/omaressameldin/lunch-roulette/internal/db"
+	"github.com/omaressameldin/lunch-roulette/internal/env"
+	"github.com/omaressameldin/lunch-roulette/pkg/commands"
+	"github.com/shomali11/slacker"
+)
+
+type Bot struct {
+	SlackBot *slacker.Slacker
+	DB       *db.DB
+}
+
+func CreateBot(d *db.DB) (*Bot, error) {
+	t := env.GetToken()
+
+	return &Bot{
+		SlackBot: slacker.NewClient(t),
+		DB:       d,
+	}, nil
+}
+
+func (b *Bot) StartListening() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	commands.AddInitCmd(b.DB, b.SlackBot)
+	commands.AddFeedCmd(b.SlackBot)
+
+	log.Println("Listening...")
+	err := b.SlackBot.Listen(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
