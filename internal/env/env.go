904e3c2603c3050d058e41d7a0cb7ac5ba44ca14
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-09 17:34:22 +0200

    Remove unneeded env variables

--- internal/env/env.go
+++ internal/env/env.go
@@ -2,20 +2,13 @@ package env
 
 import (
 	"log"
-	"strconv"
-	"time"
 
 	"github.com/omaressameldin/lunch-roulette/internal/utils"
 )
 
 func ValidateEnvKeys() {
 	GetActionPort()
-	GetRoundFrequencyPerMonth()
-	GetFirstRoundDate()
-	GetFirstRoundDate()
-	GetGroupSize()
 	GetDBName()
-	GetDBBucket()
 }
 
 func GetActionPort() string {
@@ -27,49 +20,6 @@ func GetActionPort() string {
 	return port
 }
 
-func GetRoundFrequencyPerMonth() int {
-	frequency, err := utils.GetEnv(roundFrequencyKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	frequencyNumber, err := strconv.Atoi(frequency)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return frequencyNumber
-}
-
-func GetFirstRoundDate() time.Time {
-	timeStr, err := utils.GetEnv(firstRoundDateKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	t, err := time.Parse(TimeLayout, timeStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return t
-}
-
-func GetGroupSize() int {
-	groupSize, err := utils.GetEnv(groupSizeKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	groupSizeInt, err := strconv.Atoi(groupSize)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if groupSizeInt < minGroupSize {
-		log.Fatalf("Group size must be greater than %d!", minGroupSize)
-	}
-	return groupSizeInt
-}
-
 func GetDBName() string {
 	dbName, err := utils.GetEnv(dbNameKey)
 	if err != nil {
@@ -77,15 +27,6 @@ func GetDBName() string {
 	}
 
 	return dbName
-}
-
-func GetDBBucket() string {
-	dbBucket, err := utils.GetEnv(dbBucketKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dbBucket
 }
 
 func GetToken() string {
