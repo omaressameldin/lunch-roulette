d93ea6cd4288fd15b69cb5e4d7531af8e35abbdc
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-09 11:05:41 +0200

    Change buckets to use channelId as bucket name

--- internal/db/constants.go
+++ internal/db/constants.go
@@ -1,7 +1,15 @@
 package db
 
-const latestRoundKey = "latestRound"
-const botChannelKey = "botChannel"
-const frequencyPerMonthKey = "frequencyPerMonth"
-const groupSizeKey = "groupSize"
+import "errors"
+
+//buckets
+var bucketError = errors.New("This channel is not linked to any schedules!")
+
+const keyNextRound = "nextRound"
+const keyBotChannel = "botChannel"
+const keyFrequencyPerMonth = "frequencyPerMonth"
+const keyGroupSize = "groupSize"
+const keyMembers = "members"
+
+// constants
 const timeLayout = "02.01.2006 15:04"
