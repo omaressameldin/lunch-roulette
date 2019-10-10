e6fb03730905d2eba4d3ee1a4874d2b1d0251a8d
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-10 14:10:29 +0200

    Return error if cant sleep

--- internal/utils/time.go
+++ internal/utils/time.go
@@ -8,6 +8,7 @@ import (
 func timeTill(t time.Time) (*time.Duration, error) {
 	n := time.Now()
 	d := t.Sub(n)
+
 	if n.After(t) {
 		return nil, errors.New("Time has to be in the future!")
 	}
@@ -15,14 +16,12 @@ func timeTill(t time.Time) (*time.Duration, error) {
 }
 
 func SleepTill(t time.Time) error {
-	now := time.Now()
-	if t.After(now) {
-		d, err := timeTill(t)
-		if err != nil {
-			return err
-		}
-		time.Sleep(*d)
+	d, err := timeTill(t)
+	if err != nil {
+		return err
 	}
-	return nil
 
+	time.Sleep(*d)
+
+	return nil
 }
